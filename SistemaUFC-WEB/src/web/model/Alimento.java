4e281daf338da0a18227d12192c81c7b1567da65
Ednardo <ednardo.fern@gmail.com>
2018-05-10 15:56:46 -0300

    Novos métodos

--- SistemaUFC-WEB/src/web/model/Alimento.java
+++ SistemaUFC-WEB/src/web/model/Alimento.java
@@ -1,12 +1,22 @@
 package web.model;
 
+import web.dao.AlimentoDAO;
+
 public class Alimento {
 	private String nome;
+<<<<<<< HEAD
+	private Categoria categoria = new Categoria();
+	private InforNutricional infoNutricional = new InforNutricional();
+	private AlimentoDAO alimentoDAO = AlimentoDAO.getInstancia();
+	
+	/* ******** SET's AND GET'S ***********************************************/
+=======
 	private Categoria categoria;
 	private InforNutricional infoNutricional;
 	
 	
 	
+>>>>>>> 70bbf3ca8664e1a28985ce65485ebdec96ad66ec
 	public String getNome() {
 		return nome;
 	}
@@ -26,11 +36,50 @@ public class Alimento {
 		this.infoNutricional = infoNutricional;
 	}
 	
+<<<<<<< HEAD
+	/* ******** INSERT *******************************************************/
+	public void inserirAlimento(Alimento alimento){
+		Alimento novoAlimento = new Alimento();
+		try {
+			novoAlimento.categoria.setIdCategoria(alimento.categoria.getIdCategoria());
+			novoAlimento.setNome(nome);
+			novoAlimento.infoNutricional.setInfoNutricional(novoAlimento.infoNutricional.getIdInfoNutricional());
+			alimentoDAO.inserirAlimento(novoAlimento);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	/* ******** DELETE *******************************************************/
+	public void deletarAlimento(String nome){
+		try {
+			alimentoDAO.deletarAlimento(nome);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	public Alimento() {
+=======
 
 	public Alimento(String nome, Categoria categoria, InforNutricional infoNutricional) {
 		super();
 		this.nome = nome;
 		this.categoria = categoria;
 		this.infoNutricional = infoNutricional;
+>>>>>>> 70bbf3ca8664e1a28985ce65485ebdec96ad66ec
 	}
 }
