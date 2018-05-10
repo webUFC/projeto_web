284f9c01bf18291f2bf392494b6d320eb2e48733
Ednardo <ednardo.fern@gmail.com>
2018-05-10 16:00:01 -0300

    Métodos atualizados

--- SistemaUFC-WEB/src/web/dao/AlimentoDAO.java
+++ SistemaUFC-WEB/src/web/dao/AlimentoDAO.java
@@ -5,56 +5,56 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import web.dao.conexao.ConnectionFactory;
+import web.model.Alimento;
 
 public class AlimentoDAO {
 	
-	public void inserirAlimento(){
+	private static AlimentoDAO instancia;
+	
+	/* ******** INSTANTIAT *******************************************************/
+	public static AlimentoDAO getInstancia() {
+		
+		if(instancia == null) {
+			instancia = new AlimentoDAO();
+		}
+		return instancia;
+	}
+	
+	/* ******** INSERT FOOD *******************************************************/
+	public void inserirAlimento(Alimento novoAlimento){
 		Connection con = new ConnectionFactory().getConnection();
 		
 		String sql = "INSERT INTO alimento (categoria,nomeAliment, valorNutri) VALUES (?, ?, ?)";
 		try {
 			PreparedStatement prepare = con.prepareStatement(sql);
-			prepare.setInt(1, 0);
-			prepare.setString(2, "FAZER");
-			prepare.setInt(3, 0);
+			prepare.setInt(1, (int) novoAlimento.getCategoria().getIdCategoria());
+			prepare.setString(2, novoAlimento.getNome());
+			prepare.setInt(3, novoAlimento.getInfoNutricional().getIdInfoNutricional());
 			
-			if(!prepare.execute()) {
-				System.out.println("nao deu certo");
-			}
-			else {
-				System.out.println("deu certo");
-			}
+			prepare.execute();
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-		
-		public void deletarAlimento(){
-			Connection con = new ConnectionFactory().getConnection();
+	
+	/* ******** DELETE FOOD *******************************************************/
+	public void deletarAlimento(String nome){
+		Connection con = new ConnectionFactory().getConnection();
 			
-			String sql = "DELETE FROM alimento WHERE nomeAliment = ?";
-			
-			try {
-				PreparedStatement prepare = con.prepareStatement(sql);
-				prepare.setString(1, "NOME");
-			
-				if(!prepare.execute()) {
-					System.out.println("nao deu certo");
-				}
-				else {
-					System.out.println("deu certo");
-				}
-				
+		String sql = "DELETE FROM alimento WHERE nomeAliment = ?";
+		try {
+			PreparedStatement prepare = con.prepareStatement(sql);
+			prepare.setString(1, nome);
+			prepare.execute();			
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		
-		
-	    }
-		
+	}
+	
+	/* ******** EDIT FOOD *******************************************************/
 		public void editarAlimento() {
 			Connection con = new ConnectionFactory().getConnection();
 			String sql = "UPDATE alimento SET nomeAliment = ?, categoria = ?, valorNutri=?";
@@ -90,6 +90,8 @@ public class AlimentoDAO {
 				e.printStackTrace();
 			}
 		}
+
+		
 	
 }
 	
